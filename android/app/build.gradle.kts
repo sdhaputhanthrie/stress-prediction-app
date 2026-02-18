074787d1a0b47a554b9dffaedeea7da90879fe40
sanadee_2004 <sanadeehaputhanthire@gmail.com>
2026-02-18 16:05:28 +0530

    add new plugins

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -24,7 +25,8 @@ android {
         applicationId = "com.example.stress_predition_app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        // Firebase plugins require minSdk 23 or higher. Override the Flutter default.
+        minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
